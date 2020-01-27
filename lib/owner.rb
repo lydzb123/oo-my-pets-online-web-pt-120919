e21bebd7fa01270594fb66d4c3a53d815765947d
Lydia B <lydiabaik@gmail.com>
2020-01-27 08:06:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,7 +64,7 @@ class Owner
   end
 
   def list_pets
-    puts "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    return "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 
 
